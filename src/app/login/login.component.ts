b9f3cbf288de5d7204cfa7329d841fe5fcd055d4
Adarsh Kotian <blade@example.com>
2024-08-10 21:52:15 +0530

    add changes to login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor() { } 
 
   ngOnInit(): void {
+  }
+
+  username : string = "";
+  password : string = "";
+
+  onSubmit(){
+    console.log('submit clicked');
   }
 
 }
